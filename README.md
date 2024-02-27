7c1463b9a1798aea3ba46528a29743fb6d9c162a
Vivian Davila <viviananddav@gmail.com>
2024-02-26 22:16:10 -0800

    Create JS object literals and store data for locations

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cookie-stand
+
+# Lighthouse Accessibility 
+## Monday
---
+++ js/app.js
@@ -0,0 +1,67 @@
+let hours = ["7am",
+  "8am",
+  ...];
+
+let seattle = {
+  name: "Seattle",
+  phone: "123-456-7890",
+  location: "2901 3rd Ave #300, Seattle, WA 98121",
+  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm"
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgSale: 6.3,
+  estimates: []
+}
+
+let tokyo = {
+  name: "Tokyo",
+  phone: "222-222-2222",
+  location: "1 Chome-1-2 Oshiage, Sumida City, Tokyo 131-8634",
+  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm"
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgSale: 1.2,
+  estimates: []
+}
+
+let dubai = {
+  name: "Dubai",
+  phone: "333-333-3333",
+  location: "1 Sheikh Mohammed bin Rashid Blvd - Dubai",
+  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm"
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgSale: 3.7,
+  estimates: []
+}
+
+let paris = {
+  name: "Paris",
+  phone: "444-444-4444",
+  location: "Chamo de Mars, 5 Avenue Anatole France, 75007 Paris",
+  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm"
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgSale: 2.3,
+  estimates: []
+}
+
+let lima = {
+  name: "Lima",
+  phone: "555-555-555",
+  location: "Ca. Gral. Borgoño cuadra 8, Miraflores 15074",
+  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm"
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgSale: 4.6,
+  estimates: []
+}
+
+
+//functions
+//function generateEstimate( min, max, avg ) { 
+// loop over the hours
+// random number
+// push to an array
+// return the array
+//}
