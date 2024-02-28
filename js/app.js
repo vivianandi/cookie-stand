18d6c10b6cf69853bd88dfe73a664501d5878f40
Vivian Davila <viviananddav@gmail.com>
2024-02-27 23:52:13 -0800

    Add HTML files, edit JS objects, random num gen

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
+let hours = "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm"
+
 let seattle = {
-  name: "Seattle",
-  phone: "123-456-7890",
-  location: "2901 3rd Ave #300, Seattle, WA 98121",
-  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
   minCustomers: 23,
   maxCustomers: 65,
   avgSale: 6.3,
@@ -10,10 +8,6 @@ let seattle = {
 }
 
 let tokyo = {
-  name: "Tokyo",
-  phone: "222-222-2222",
-  location: "1 Chome-1-2 Oshiage, Sumida City, Tokyo 131-8634",
-  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
   minCustomers: 3,
   maxCustomers: 24,
   avgSale: 1.2,
@@ -21,10 +15,6 @@ let tokyo = {
 }
 
 let dubai = {
-  name: "Dubai",
-  phone: "333-333-3333",
-  location: "1 Sheikh Mohammed bin Rashid Blvd - Dubai",
-  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
   minCustomers: 11,
   maxCustomers: 38,
   avgSale: 3.7,
@@ -32,10 +22,6 @@ let dubai = {
 }
 
 let paris = {
-  name: "Paris",
-  phone: "444-444-4444",
-  location: "Chamo de Mars, 5 Avenue Anatole France, 75007 Paris",
-  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
   minCustomers: 20,
   maxCustomers: 38,
   avgSale: 2.3,
@@ -43,10 +29,6 @@ let paris = {
 }
 
 let lima = {
-  name: "Lima",
-  phone: "555-555-555",
-  location: "Ca. Gral. Borgoño cuadra 8, Miraflores 15074",
-  hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
   minCustomers: 2,
   maxCustomers: 16,
   avgSale: 4.6,
@@ -64,6 +46,13 @@ let lima = {
 // push to an array
 // return the array
 //}
+
+//random number
+function randomNumberGenerator(minCustomers, maxCustomers) {
+  return Math.floor(
+    Math.random() * (maxCustomers - minCustomers + 1) + minCustomers
+  );
+}
 
 // let allCities = [seattle, tokyo, dubai, paris, lima]
 let allCities = [];
